34ff8454d48d341e95124b68c9c63614c753dbb4
Cherif Diallo <riftone07@gmail.com>
2024-05-26 15:06:44 +0000

    changement et ajout des utilisateur fraude

---
+++ database/migrations/2024_05_26_145134_create_transactions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('date_de_transaction');
+            $table->bigInteger('montant');
+            $table->foreignId('boutique_id')->constrained('boutiques');
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
