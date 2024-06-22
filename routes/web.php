7613704452af6717c2d94843ed13e641c501373d
Cherif Diallo <riftone07@gmail.com>
2024-06-22 22:24:21 +0000

    chsn

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+
+Route::resource('decaissements',\App\Http\Controllers\DecaissementController::class);
